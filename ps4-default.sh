c0309545edc794ac1f03882fad4a591d3ea18f4f
Sajad Torkamani <sajadtorkamani1@gmail.com>
2025-12-14 15:33:02 +0000

    wip

---
+++ ps4-default.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eoux pipefail
+
+name=Sajad
+age=31
+
+echo "Hello, my name is $name and I'm $age years old"
---
+++ template.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# <Describe what this script does>
+
+# Turn on strict mode
+# -e exit the script if any command returns with a non-zero status code
+# -u treat use of undefined variables as an error
+# -o Consider a pipeline failed if any of its commands fail not just the last one
+set -euo pipefail
+
+echo "TODO"
+
+# Explicitly specify an exit code otherwise the exit code of the last executed
+# command will be used as the exit code which may not be what you want.
+exit 0
+
