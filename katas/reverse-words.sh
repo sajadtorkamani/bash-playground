59ee4d012cf7fd91456ae0f951008d69316a62ff
Sajad Torkamani <sajadtorkamani1@gmail.com>
2025-12-20 13:20:32 +0000

    wip

--- katas/reverse-words.sh
+++ katas/reverse-words.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+set -euo pipefail
 
 # Reverse the words in the given string
 function reverse_words() {
   local str="$1"
   local words
-  read -ra words <<< "$str"
+  read -ra words <<<"$str"
   local num_words="${#words[@]}"
-  reversed_words=()
+  local reversed_words=()
 
-  for (( index=num_words-1; index >= 0; index-- )); do
+  for ((index = num_words - 1; index >= 0; index--)); do
     reversed_words+=("${words[$index]}")
   done
 
- for word in $reversed_words; do
-#   echo "$word"
-   echo "$word"
- done
+  echo "${reversed_words[*]}"
 }
 
 reverse_words "hello how are you doing?"
